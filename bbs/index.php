216c26d4abf44d70e6c705dd2db0a9043c54024f
shiftcnArchive <152615499+shiftcn-yintsangArchive@users.noreply.github.com>
2025-10-11 05:45:44 +0800

    Create index.php

---
+++ bbs/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+
+<head>
+
+
+    <meta charset="UTF-8">
+
+
+    <meta http-equiv="refresh" content="3;url=https://shiftcn-yintsangarchive.github.io/bbs.html">
+
+
+    <title>页面跳转</title>
+
+
+</head>
+
+
+<body>
+
+
+    <p>页面将在3秒后跳转到论坛</p>
+
+
+</body>
+
+
+</html>
+
